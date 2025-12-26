6561dca51a2b9b93c3ef152a4d066478f51b0102
Daniel Magee <danielmagee1234@gmail.com>
2025-12-26 05:06:12 -0500

    basic beginning

---
+++ app/header.js
@@ -0,0 +1,17 @@
+
+export default function Header() {
+    return (
+        <header>
+            <h1>MCAT Tracker</h1>
+            <nav>
+                <ul>
+                    <li><a href = "#">Home</a></li>
+                    <li>Progress</li>
+                    <li>Calendar</li>
+                    <li>Community</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
