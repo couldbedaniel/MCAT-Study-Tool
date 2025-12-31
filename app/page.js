41e1371c8b896755aa6e5cef6987c8bce02459b2
Daniel Magee <danielmagee1234@gmail.com>
2025-12-31 01:23:11 -0500

    begin score tracking

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
-import Header from "./header.js"
-
+import Header from "./header.js";
+import Scores from "./scores.js";
 
 export default function Home() {
   return (
-     <Header></Header>
+     <>
+     <Scores></Scores>
+     </>
   );
 }
---
+++ app/scores.js
@@ -0,0 +1,13 @@
+export default function Scores() {
+    return (
+        <div className = "scores">
+            <img src = "\graphexample.png"></img>
+            <h3>Best Score</h3>
+            <h1>520</h1>
+            <h3>Improvement</h3>
+            <h1>10</h1>
+            <h3>Most Recent Score</h3>
+            <h1>518</h1>
+        </div>
+    )
+}
